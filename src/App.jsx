19b29ed79b5088f2e4b0f88ef3d652e152068a92
Guta <guttir19@gmail.com>
2026-09-20 22:59:53 +0300

    navbar and fetched API

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+import Navbar from "./components/navbar";
+
+function App(){
+    const[products,setProducts]=useState([])
+
+    useEffect(()=>{  
+    async function getProducts() {
+      try {
+        const response = await fetch(
+          "https://fakestoreapi.com/products"
+        );
+        if (!response.ok) {
+          throw new Error("Failed to fetch products");
+        }
+        const data = await response.json();                
+        setProducts(data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    getProducts();  
+},[]);
+    
+    return(
+        <>
+        <Navbar />
+        </>
+    );
+};
+
+export default App
