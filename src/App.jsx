1037bb33d9904989a1cd4f381a37ba8a57bc0361
joabera333 <johannesab0939@gmail.com>
2026-09-22 12:11:05 +0300

    feat: add About page and route to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/home";
+import About from "./pages/About";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <main className="flex-1">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
         </Routes>
       </main>
     </div>
